2313e1e9584ed40fae2f892aacc08ff83493ce19
Sta Rog <satach@o2.pl>
2023-08-31 18:55:15 +0200

    loading data finished

--- data_functions.py
+++ data_functions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Lista nazw aktywów
-tickers = ["cdr", "abe", "bcx"]  # Dodaj tutaj nazwy aktywów
+tickers = ["cdr", "tim", "pko"]  # Dodaj tutaj nazwy aktywów
 
 # Tworzenie listy nazw plików na podstawie nazw aktywów
 file_names = [f"data/{ticker}.txt" for ticker in tickers]
@@ -19,16 +19,22 @@ for file_name, ticker in zip(file_names, tickers):
             _, _, date, _, _, _, _, close, _, _ = parts
             data.append((ticker, date, float(close)))
     
-    df_ticker = pd.DataFrame(data, columns=["Ticker", "Date", f"{ticker}_Close"])
+    df_ticker = pd.DataFrame(data, columns=["Ticker", "Date", f"{ticker}"])
     df_ticker["Date"] = pd.to_datetime(df_ticker["Date"], format="%Y%m%d")
     df = pd.merge(df, df_ticker, on=["Ticker", "Date"], how="outer")
 
 # Dodawanie kolumny "Month" z formatem roku i miesiąca
 df["Month"] = df["Date"].dt.to_period("M")
 
-# Grupowanie po miesiącu i obliczanie średniej dla kolumn "Close" dla różnych aktywów
-columns_to_group = [f"{ticker}_Close" for ticker in tickers]
-columns_to_group.insert(0, "Month")
+# Lista kolumn do grupowania, bez kolumny "Month"
+columns_to_group = [f"{ticker}" for ticker in tickers]
 monthly_average = df.groupby("Month")[columns_to_group].mean().reset_index()
 
-print(monthly_average)
+# Usuwanie wierszy z miesiącami, w których nie wszystkie aktywa mają wartości
+all_tickers_columns = set(df.columns)
+missing_data_months = monthly_average[monthly_average[columns_to_group].isnull().any(axis=1)]["Month"]
+valid_months = monthly_average[~monthly_average["Month"].isin(missing_data_months)]
+if valid_months.empty:
+    print("Aktywa nie współistnieją.")
+else:
+    print(valid_months)
