05261336654ee775f9378d4cea9aa411b6145465
Sta Rog <satach@o2.pl>
2023-09-02 19:14:39 +0200

    1

---
+++ portfolio/models.py
@@ -0,0 +1,23 @@
+# assets/models.py
+from django.db import models
+
+class Asset(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Portfolio(models.Model):
+    name = models.CharField(max_length=50)
+    assets = models.ManyToManyField(Asset, through='PortfolioAsset')
+
+    def __str__(self):
+        return self.name
+
+class PortfolioAsset(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    weight = models.FloatField()
+
+    def __str__(self):
+        return f"{self.asset.name} ({self.weight}%)"
