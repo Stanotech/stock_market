f800e1e25fca4f3b286fb50ab87527dce7bd4843
Sta Rog <satach@o2.pl>
2023-09-09 07:08:05 +0200

    organizing data functions

--- portfolio/data_functions.py
+++ portfolio/data_functions.py
@@ -4,6 +4,8 @@ import pandas as pd
 from datetime import datetime
 from portfolio.models import Asset, AssetValue
 from django.db.models import Max, Min
+import numpy as np
+from cvxpy import *
 
 class DataFunctions:
 
@@ -86,3 +88,61 @@ class DataFunctions:
         df = pd.DataFrame(data)
 
         return df
+    
+    @classmethod
+    def Markovitz(cls):
+    
+        mp = PrepareData(["cdr", "tim", "pko"]).set_index("Month")
+        mr = pd.DataFrame()
+
+
+        # compute monthly returns
+
+        for s in mp.columns:
+            date = mp.index[0]
+            pr0 = mp[s][date] 
+            for t in range(1,len(mp.index)):
+                date = mp.index[t]
+                pr1 = mp[s][date]
+                ret = (pr1-pr0)/pr0
+                mr._set_value(date,s,ret)
+                pr0 = pr1
+
+
+        r = np.asarray(np.mean(mr, axis=0))
+        C = np.asmatrix(np.cov(mr, rowvar=False))
+
+
+        # Get symbols
+        symbols = mr.columns
+
+        # Number of variables
+        n = len(symbols)
+
+        # The variables vector
+        x = Variable(n)
+
+        # The minimum return
+        req_return = 0.02
+
+        # The return
+        ret = r.T@x
+
+        # The risk in xT.Q.x format
+        risk = quad_form(x, C)
+
+        # The core problem definition with the Problem class from CVXPY
+        prob = Problem(Minimize(risk), [sum(x)==1, ret >= req_return, x >= 0])
+
+
+        try:
+            prob.solve()
+            print ("Optimal portfolio")
+            print ("----------------------")
+            for s in range(len(symbols)):
+               print (" Investment in {} : {}% of the portfolio".format(symbols[s],round(100*x.value[s],2)))
+            print ("----------------------")
+            print ("Exp ret = {}%".format(round(100*ret.value,2)))
+            print ("Expected risk    = {}%".format(round(100*risk.value**0.5,2)))
+        except:
+            print ("Error")
