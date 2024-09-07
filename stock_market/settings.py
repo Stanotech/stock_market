a79324dcf31d139feb3756c9e84a6ccfb1d64915
Sta Rog <satach@o2.pl>
2024-09-07 23:35:35 +0200

    deploy

--- stock_market/settings.py
+++ stock_market/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "}y>LeuL`.v~>X}VN'y^]`|:x@*(&_f)`)h/jubjFgWx&9Ixvr'"
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("IS_DEVELOPMENT", True)
